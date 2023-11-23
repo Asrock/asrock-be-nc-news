c961daa665934bcfbb0a29cce65990c8fa0e5ad1
Asrock <soad@jerez.es>
2023-11-23 17:12:22 +0000

    minor fix to db connection file

--- db/connection.js
+++ db/connection.js
@@ -14,5 +14,5 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const db = new Pool()
+const db = new Pool(config)
 module.exports = db;
